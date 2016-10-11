b677b1672b01da6964d7ef0448cde402dc9ef7da
othmaro <bquevedo50@gmail.com>
2016-10-10 21:05:06 -0600

    Add files via upload

---
+++ Billy/proyecto_P1.py
@@ -0,0 +1,33 @@
+#*-* coding: utf-8 -*-
+import os
+
+def IngresarNumero(dato):
+	try:							
+		int(dato)
+		retorno = True
+	except ValueError:
+		retorno = False
+	return(retorno)	
+
+def TorreHanoi(discos, inicio = 1, fin = 3):
+	if discos == 1:
+		print "Mueve el disco en la columna %d a la columna %d" % (inicio,fin)
+	else:
+		TorreHanoi(discos - 1, inicio, 6 - inicio - fin)
+		print "Mueve el disco en la columna %d a la columna %d" % (inicio, fin)
+		TorreHanoi(discos - 1, 6 - inicio - fin, fin)
+		
+Numero = raw_input("Ingresa el numero de discos que deseas ver la solucion: ")
+while True:
+	if IngresarNumero(Numero) == True:
+		Numero = int(Numero)
+		if Numero <= 1:
+			os.system("cls")
+			Numero = raw_input("Ingresa un numero de discos valido: ")
+		else:
+			os.system("cls")
+			TorreHanoi(Numero)
+			break
+	else:
+		os.system('cls')
+		Numero = raw_input("Ingresa un numero de discos valido: ")
