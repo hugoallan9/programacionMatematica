6e19ce1339108c1097d590b43009aa9f62dfc08d
Hugo Garcia <hugoallangm@gmail.com>
2016-11-02 16:17:17 -0600

    Avances en quicksort
    Codigo

--- skeleton/App/ordenamiento.py
+++ skeleton/App/ordenamiento.py
@@ -55,13 +55,41 @@ def mergeSort(lista):
             resultado = merge(izquierda,derecha)
             return resultado
 
+def quicksort(lista, inicial, final):
+    if inicial < final:
+        return 0
+        
+            
+            
+def quick(lista, inicial, final):
+    pivote = lista[final]
+    izquierdo = inicial
+    derecho = final-1
+    hecho = True
+    aux = 0
+    while hecho:
+        while lista[izquierdo] <= pivote:
+            izquierdo = izquierdo + 1
+        while lista[derecho] >= pivote and derecho >= izquierdo:
+            derecho = derecho -1
+        if derecho < izquierdo:
+            hecho = False
+        else:
+            aux = lista[izquierdo]
+            lista[izquierdo] = lista[derecho]
+            lista[derecho] = aux
+            print lista
+    aux = lista[izquierdo]
+    lista[izquierdo] = pivote
+    lista[final] = aux
+    return lista, izquierdo, derecho
     
-if __name__ == "__main__":
-    l = range(10000)
-    burbuja1(l)
-    tiempo_inicio = clock()
-    l1 = mergeSort(l)
-    tiempo_fin = clock()
-    print 'Con un tiempo', tiempo_fin - tiempo_inicio
+#if __name__ == "__main__":
+    #l = range(10000)
+    #burbuja1(l)
+    #tiempo_inicio = clock()
+    #l1 = mergeSort(l)
+    #tiempo_fin = clock()
+    #print 'Con un tiempo', tiempo_fin - tiempo_inicio
     
                 
