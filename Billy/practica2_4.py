02ab0b88a0154e1865e8eba1434198672b2ddf16
othmaro <bquevedo50@gmail.com>
2016-11-20 20:11:55 -0600

    Update practica2_4.py

--- Billy/practica2_4.py
+++ Billy/practica2_4.py
@@ -1,16 +1,17 @@
+#problema 87
 import eulerlib, math
 
-primos = eulerlib.primes(math.sqrt(50)) #Lista de numeros primos menores que la raiz cuadrada de 50000000
+primos = eulerlib.primes(math.sqrt(50000000)) #Lista de numeros primos menores que la raiz cuadrada de 50000000
 numeros = {0}
 
 for i in range(2,5): #Recorre los posibles exponentes para los primos [2,3,4]
 	numerosaux = set()
 	for p in primos: 
 		q = p ** i #Prueba las posibles combinaciones entre exponentes y primos
-		if q > 50:
+		if q > 50000000:
 			break
 		for x in numeros:
-			if x + q <= 50:
+			if x + q <= 50000000:
 				numerosaux.add(x + q)
 	numeros = numerosaux
 
